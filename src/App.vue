10e2b2e08b22d5a9920baf24dfc7bee11560f3a8
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-12-03 18:18:31 -0700

    Add basic todo components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,48 @@
 <template>
   <div>
-    <h1>Calendar list</h1>
+    <Title />
+    <label><input
+      v-model="todoItem"
+      placeholder="Enter Todo Item..."><button @click="addTodo()">Add</button></label>
+    <p v-if="todos.length">
+      <ul
+        v-for="todo in todos"
+        :key="todo.id"
+        style="padding: 0">
+        <label><input
+          :checked="todo.completed"
+          type="checkbox"
+          style="margin-right: 5px"
+          @change="completeTodo(todo)">{{ todo.description }}</label>
+      </ul>
+    </p>
+    <p v-else>
+      There are no todo items yet
+    </p>
   </div>
 </template>
+
+<script>
+import Title from './Title.vue'
+export default {
+  components: {
+    Title
+  },
+  data () {
+    return {
+      title: 'Simple Todo List',
+      todoItem: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo: function () {
+      this.todos.push({ description: this.todoItem, completed: false, id: this.todos.length })
+      this.todoItem = ''
+    },
+    completeTodo: function (todo) {
+      todo.isComplete = !todo.isComplete
+    }
+  }
+}
+</script>
